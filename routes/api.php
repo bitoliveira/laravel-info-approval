7b44242383944452de8dea55e887e4ad52a4132b
Bruno Oliveira <bit.oliveira.dev@gmail.com>
2025-11-16 22:04:12 +0000

    wip

--- routes/api.php
+++ routes/api.php
@@ -7,8 +7,8 @@ Route::group([
     'prefix' => config('approval.api.prefix', 'approvals'),
     'middleware' => config('approval.api.middleware', ['api']),
 ], function () {
-    Route::get('/', [ApprovalApiController::class, 'index'])->name('approval.api.index');
-    Route::get('/{approval}', [ApprovalApiController::class, 'show'])->name('approval.api.show');
-    Route::post('/{approval}/approve', [ApprovalApiController::class, 'approve'])->name('approval.api.approve');
-    Route::post('/{approval}/reject', [ApprovalApiController::class, 'reject'])->name('approval.api.reject');
+    Route::get('/', [ApprovalApiController::class, 'index'])->name('api.approval.index');
+    Route::get('/{approval}', [ApprovalApiController::class, 'show'])->name('api.approval.show');
+    Route::post('/{approval}/approve', [ApprovalApiController::class, 'approve'])->name('api.approval.approve');
+    Route::post('/{approval}/reject', [ApprovalApiController::class, 'reject'])->name('api.approval.reject');
 });
